c4af4974bb1c87241be009903a38bda0b4284e0a
shidephen <shidephen@outlook.com>
2018-07-06 09:15:04 +0800

    Change to keras engine.

--- egs/slt_arctic/s1/04_train_acoustic_model.sh
+++ egs/slt_arctic/s1/04_train_acoustic_model.sh
@@ -18,6 +18,6 @@ acoustic_conf_file=$1
 ### Step 4: train acoustic model ###
 echo "Step 4:"
 echo "training acoustic model..."
-./scripts/submit.sh ${MerlinDir}/src/run_merlin.py $acoustic_conf_file
+./scripts/submit.sh ${MerlinDir}/src/run_keras_with_merlin_io.py $acoustic_conf_file
 
 
